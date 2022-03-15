67e6c777d5a645cb0a1263b28dbe79404d2d86b3
Tautvydas Tunkis <ttunkis@gmail.com>
2022-03-15 16:11:55 +0200

    +

---
+++ src/model/user.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    name: 'James',
+    email: 'James@Bond.com',
+    age: '28',
+    gender: 'male',
+  },
+  {
+    name: 'Bob',
+    email: 'Bob@gmail.com',
+    age: '25',
+    gender: 'male',
+  },
+  {
+    name: 'Jane',
+    email: 'Jae@doe.com',
+    age: '30',
+    gender: 'female',
+  },
+];
+
+function getUsers() {
+  return users;
+}
+module.exports = {
+  getUsers,
+};
--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
+const staticPath = path.join(__dirname, 'assets');
+app.use(express.static(staticPath));
+
 // middleware
 app.use(morgan('dev'));
 app.use(cors());
@@ -21,9 +24,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
   // const kelias = path.join(__dirname, 'views', 'index.html');
   // res.sendFile(kelias);
+
   const data = {
     title: 'Homepage 1',
     age: 25,
+    page: 'home',
   };
   res.render('index', data);
 });
@@ -32,8 +37,25 @@ app.get('/about', (req, res) => {
   const data = {
     title: 'About us',
     tech: ['HTML', 'CSS', 'JS', 'Node'],
+    page: 'about',
   };
   res.render('about', data);
+});
+
+app.get('/contact', (req, res) => {
+  const data = {
+    title: 'Contact us',
+    page: 'contact',
+  };
+  res.render('contact', data);
+});
+
+app.all('*', (req, res) => {
+  const data = {
+    title: '404 not found',
+    page: '404',
+  };
+  res.render('404', data);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
